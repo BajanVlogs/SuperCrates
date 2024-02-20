efc3eff191a063d718f3fdb6b344eed92674e92c
BajanVlogs <kjolly544@gmail.com>
2024-02-19 20:55:01 -0500

    fix issue

--- src/BajanVlogs/SuperCrates/Main.php
+++ src/BajanVlogs/SuperCrates/Main.php
@@ -6,23 +6,16 @@ use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\entity\Entity;
-use pocketmine\event\block\BlockEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\Item;
-use pocketmine\level\sound\PopSound;
-use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\AddItemEntityPacket;
-use pocketmine\network\mcpe\protocol\AddPlayerPacket;
-use pocketmine\network\mcpe\protocol\BlockEventPacket;
-use pocketmine\network\mcpe\protocol\RemoveEntityPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\UUID;
+use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener {
     public $Crate = [];
@@ -35,14 +28,14 @@ class Main extends PluginBase implements Listener {
     /** @var Config */
     public $items;
 
-    public function onEnable(){
+    public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
         $this->saveResource("items.yml");
         $this->items = new Config($this->getDataFolder() . "items.yml", Config::YAML);
     }
 
-    public function onPlayerJoin(PlayerJoinEvent $event){
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
         foreach($this->locations as $crate => $xyz){
             $xyz = explode(":", $xyz);
@@ -51,7 +44,7 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-    public function onPlayerInteract(PlayerInteractEvent $event){
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
         $player = $event->getPlayer();
         $block = $event->getBlock();
         $pos = new Vector3($block->getX(), $block->getY(), $block->getZ());
@@ -67,12 +60,12 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-    public function onBlockPlace(BlockPlaceEvent $event){
+    public function onBlockPlace(BlockPlaceEvent $event): void {
         $block = $event->getBlock();
         $this->getChestReady($block);
     }
 
-    public function spawnCrate(string $crate, Vector3 $position, Player $player){
+    public function spawnCrate(string $crate, Vector3 $position, Player $player): void {
         $pk = new AddPlayerPacket();
         $pk->uuid = UUID::fromRandom();
         $pk->username = "Chest";
@@ -94,14 +87,13 @@ class Main extends PluginBase implements Listener {
         $this->Crate[$position->getX() . ":" . $position->getY() . ":" . $position->getZ() . "TEXT"] = $id;
     }
 
-    public function handleCrateInteraction(Player $player, string $crate, Block $block, Item $item){
+    public function handleCrateInteraction(Player $player, string $crate, Block $block, Item $item): void {
         // Handle crate interaction logic here
     }
 
-    public function getChestReady(Block $block){
+    public function getChestReady(Block $block): void {
         // Get chest ready logic
     }
 
     // Other methods...
 }
-
